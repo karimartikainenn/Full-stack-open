f40bca41936ee25a8af6c6304a028f22a8d8d486
kari.martikainenn <kari_martikainen@icloud.com>
2024-01-26 14:54:30 +0200

    1.2: kurssitiedot, step2

---
+++ osa1/kurssitiedot/part1/src/App.jsx
@@ -0,0 +1,45 @@
+const Header = (props) => {
+  return ( <h1>{props.course}</h1> )
+}
+
+const Content = (props) => {
+  return (
+    <p>
+      <Part />
+      <Part />
+      <Part />
+    </p>
+  )
+}
+
+const Total = (props) => {
+  return ( <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p> )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course = {course} />
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+
+export default App
