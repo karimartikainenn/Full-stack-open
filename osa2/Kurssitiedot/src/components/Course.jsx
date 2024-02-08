5c0ebaca4e061ed31105799efed20fbe5edf063a
Kari <kari_martikainen@icloud.com>
2024-02-08 16:29:27 +0200

    erillinen moduuli valmis

--- osa2/Kurssitiedot/src/components/Course.jsx
+++ osa2/Kurssitiedot/src/components/Course.jsx
@@ -1,21 +1,61 @@
 import React from "react";
 
-const Course = ({ course }) => {
+const Course = () => {
+const courses = [
+    {
+        name: 'Half Stack application development',
+        id: 1,
+        parts: [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10,
+            id: 1
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7,
+            id: 2
+        },
+        {
+            name: 'State of a component',
+            exercises: 14,
+            id: 3
+        },
+        {
+            name: 'Redux',
+            exercises: 11,
+            id: 4
+        }
+        ]
+    }, 
+    {
+        name: 'Node.js',
+        id: 2,
+        parts: [
+        {
+            name: 'Routing',
+            exercises: 3,
+            id: 1
+        },
+        {
+            name: 'Middlewares',
+            exercises: 7,
+            id: 2
+        }
+        ]
+    }
+    ]
+    
   return (
     <div>
-      <h1>{course.name}</h1>
-      {course.parts.map((part) => (
-        <p key={part.id}>
-          {part.name} {part.exercises}
-        </p>
-      ))}
-      <p>
-        <strong>
-          total of{" "}
-          {course.parts.reduce((sum, part) => sum + part.exercises, 0)}{" "}
-          exercises
-        </strong>
-      </p>
+      {courses.map(course => 
+        <div key={course.id}>
+          <h1>{course.name}</h1>
+          {course.parts.map(part => 
+            <p key={part.id}>{part.name} {part.exercises}</p>
+          )}
+        </div>
+      )}
     </div>
   );
 };
