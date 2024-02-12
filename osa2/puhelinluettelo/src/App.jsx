b470e2d13a7de70eaeabfbc588a78583a67bdaff
Kari <kari_martikainen@icloud.com>
2024-02-12 18:47:38 +0200

    puhelinluettelo step1 valmis

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addName = (e) => {
+    e.preventDefault();
+    const nameObject = {
+      name: newName,
+      id: persons.length + 1,
+    };
+    setPersons(persons.concat(nameObject));
+    setNewName("");
+  };
+
+  const handleNoteChange = (e) => {
+    console.log(e.target.value);
+    setNewName(e.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNoteChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <div key={person.name}>{person.name}</div>
+      ))}
+      
+    </div>
+  );
+};
+
+export default App;
