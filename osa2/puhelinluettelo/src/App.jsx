0f3dcc200272f6d0c80446ee27cdf8f09d6b0029
Kari <kari_martikainen@icloud.com>
2024-02-17 23:09:18 +0200

    Pieni muutos

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -5,12 +5,7 @@ import Filter from "./components/Filter";
 import Render from "./components/Render";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", id: 1, number: "040123456" },
-    { name: "Ada Lovelace", id: 2, number: "0452359411" },
-    { name: "Dan Abramov", id: 3, number: "0491209455" },
-    { name: "Kari Martikainen", id: 4, number: "0452350455" },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   useEffect(() => {
     console.log('effect')
