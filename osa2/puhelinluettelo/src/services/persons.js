25fb6dfe258f4bdebe77c3fcdaac32057f1878aa
Kari <kari_martikainen@icloud.com>
2024-02-26 22:09:35 +0200

    persons.js update

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -1,20 +1,37 @@
-import axios from 'axios'
-const baseUrl = '/api/persons'
+import axios from 'axios';
+
+const baseUrl = '/api/persons';
 
 const getAll = () => {
   return axios.get(baseUrl)
+    .catch(error => {
+      console.error('Error fetching data:', error);
+      throw error; 
+    });
 }
 
 const create = newObject => {
   return axios.post(baseUrl, newObject)
+    .catch(error => {
+      console.error('Error creating data:', error);
+      throw error; 
+    });
 }
 
 const update = (id, newObject) => {
   return axios.put(`${baseUrl}/${id}`, newObject)
+    .catch(error => {
+      console.error('Error updating data:', error);
+      throw error; 
+    });
 }
 
 const remove = id => {
-  return axios.delete(`${baseUrl}/${id}`);
+  return axios.delete(`${baseUrl}/${id}`)
+    .catch(error => {
+      console.error('Error deleting data:', error);
+      throw error; 
+    });
 };
 
 export default { 
@@ -22,4 +39,4 @@ export default {
   create: create, 
   update: update,
   remove: remove, 
-}
+};
